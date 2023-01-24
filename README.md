a0abd4d3d8353328564c734529db4818ac39e180
Marco.Barreto <marco.agbarreto@gmail.com>
2023-01-23 21:53:18 -0600

    replaced imageio and pygifsicle with PIL

--- README.md
+++ README.md
@@ -2,18 +2,18 @@
 
 This is a program to generate gif images given a path with images.
 
-* Download the latest release of [gif-generator.exe](https://github.com/marcoagbarreto/gif-generator/releases/download/v0.1.0/gif-generator.exe)
+* Download the latest release of [gif-generator.exe](https://github.com/marcoagbarreto/gif-generator/releases/download/v0.1.1/gif-generator.exe)
 
 ## Example
 
 1. Go to the path where the ``gif-generator.exe`` or ``gif-generator.py`` is stored.
 2. Open ``cmd`` and go to the ``gif-generator.exe`` folder.
 3. Use these commands to run ``gif-generator.exe``.
-4. ``C:\path_to_app\>gif-generator.exe -n <file_name> -p <path> -f <fps>``
+4. ``C:\path_to_app\>gif-generator.exe -n <file_name> -p <path> -f <fps> -q <quality>``
 5. Profit.
 
-* ``C:\gif-generator>gif-generator.exe -n pixelspatterns -p "C:\Users\user\Downloads\images" -f 5``
-* ``C:\gif-generator>python gif-generator.py -n pixelspatterns -p "C:\Users\user\Downloads\images" -f 5``
+* ``C:\gif-generator>gif-generator.exe -n pixelspatterns -p "C:\Users\user\Downloads\images" -f 5 -q 75``
+* ``C:\gif-generator>python gif-generator.py -n pixelspatterns -p "C:\Users\user\Downloads\images" -f 5 -q 75``
 
 ![example](pixelspatterns.gif)
 
@@ -27,17 +27,13 @@ dependencies:
 
     import sys
     import getopt
-    import imageio.v2 as imageio
     import os
-    from pygifsicle import optimize
+    from PIL import Image
+
 
 or [download and extract the zip](https://github.com/marcoagbarreto/gif-generator/archive/main.zip) into your project folder.
 
-## Known limitations:
-* If running with python, and you are on Windows, you need to add gifsicle to your environment variables for pygifsicle to work.
-  * Example: Path = C:\Program Files\gifsicle 
-  * [Video](https://www.youtube.com/watch?v=5gdhQyP9Eog&t=125s)
-  * https://www.lcdf.org/gifsicle/ 
+## Known limitations: 
 * If you want the GIF to follow a specific sequence, name the images numerically: 1.png, 2.png, 3.png...
 * Supported image formats: ``".png", ".jpg", ".jpeg"``
 
--- gif-generator.py
+++ gif-generator.py
@@ -1,23 +1,25 @@
 import sys
 import getopt
-import imageio.v2 as imageio
 import os
-from pygifsicle import optimize
+from PIL import Image
 
 default_fps = 60
+default_quality = 95
 
 
 def parse_args(argv):
     arg_file_name = ""
     arg_path = ""
     arg_fps = ""
-    arg_help = "{0} -n <file_name> -p <path> -f <fps>".format(argv[0])
+    arg_quality = ""
+    arg_help = "{0} -n <file_name> -p <path> -f <fps> -q <quality>".format(argv[0])
 
     try:
-        opts, args = getopt.getopt(argv[1:], "hn:p:f:", ["help",
-                                                         "file_name=",
-                                                         "path=",
-                                                         "fps="])
+        opts, args = getopt.getopt(argv[1:], "hn:p:f:q:", ["help",
+                                                           "file_name=",
+                                                           "path=",
+                                                           "fps=",
+                                                           "quality"])
     except:
         print(arg_help)
         sys.exit(2)
@@ -32,6 +34,8 @@ def parse_args(argv):
             arg_path = arg
         elif opt in ("-f", "--fps"):
             arg_fps = arg
+        elif opt in ("-q", "--quality"):
+            arg_quality = arg
 
     print('file_name:', arg_file_name)
     print('path:', arg_path)
@@ -40,8 +44,13 @@ def parse_args(argv):
     else:
         arg_fps = default_fps
         print('fps:', arg_fps, '*default')
+    if arg_quality:
+        print('quality:', arg_quality)
+    else:
+        arg_quality = default_quality
+        print('quality:', arg_quality, '*default')
 
-    return arg_file_name, arg_path, int(arg_fps)
+    return arg_file_name, arg_path, int(arg_fps), int(arg_quality)
 
 
 def append_images(folder_images):
@@ -53,21 +62,24 @@ def append_images(folder_images):
     return images
 
 
-def make_gif(file_name, path, fps=default_fps):
-    # fps: change this for GIF speed. The higher, the faster
-    images = append_images(path + '/')
+def make_gif(file_name, path, fps=default_fps, quality=default_quality):
+    # Specify the directory containing the images
+    images = append_images(path)
     gif_path = path + '/' + file_name + '.gif'
-    with imageio.get_writer(gif_path, mode='I', fps=fps) as writer:
-        for image in images:
-            writer.append_data(imageio.imread(image))
-
-    optimize(gif_path)
+    # Create a list of image filenames
+    frames = [Image.open(image).convert('RGB') for image in images]
+    # Save the frames as an optimized GIF
+    frames[0].save(gif_path,
+                   format='GIF',
+                   append_images=frames[1:],
+                   save_all=True,
+                   duration=(fps * 1000) / len(frames),
+                   loop=0,
+                   optimize=True,
+                   quality=quality)
 
 
 if __name__ == "__main__":
-    arg_file_name, arg_path, arg_fps = parse_args(sys.argv)
+    arg_file_name, arg_path, arg_fps, arg_quality = parse_args(sys.argv)
 
-    if arg_fps:
-        make_gif(file_name=arg_file_name, path=arg_path, fps=arg_fps)
-    else:
-        make_gif(file_name=arg_file_name, path=arg_path)
+    make_gif(file_name=arg_file_name, path=arg_path, fps=arg_fps, quality=arg_quality)
